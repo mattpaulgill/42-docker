6efab70b39b2c611eb1b47dd5b0ae1cdd90947e5
mattpaulgill <mattpaulgill@gmail.com>
2018-06-23 02:46:10 -0700

    Update main.js

--- main.js
+++ main.js
@@ -12,4 +12,4 @@ http.createServer(function (request, response) {
 }).listen(8080);
 
 // Console will print the message
-console.log('Server running at http://127.0.0.1:8080/');
+console.log('Server running!');
